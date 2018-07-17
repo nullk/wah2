ece3dfe5958fa37b9c762d0007f5fbbb5f2ebdca
nullk <nullk@protonmail.ch>
2018-07-17 19:57:15 -0400

    Video duration display

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,10 @@
+/*
+
+For most of the code in this file, credit goes to:
+https://demo.tutorialzine.com/2015/08/how-to-control-youtubes-video-player-with-javascript/
+
+*/
+
 var player;
 var socket;
 
@@ -26,11 +33,13 @@ window.initialize = function(event) {
     $("#page-title").html("<a href='https://www.youtube.com/watch?v="+event.target.getVideoData()["video_id"]+"'>"+event.target.getVideoData()["title"]+"</a>");
     $("#page-author").html('By ' + event.target.getVideoData()["author"]);
     updateProgressBar();
+    updateTimerDisplay()
 
     // Start interval to update elapsed time display and
     // the elapsed part of the progress bar every second.
     var time_update_interval = setInterval(function () {
         updateProgressBar();
+        updateTimerDisplay()
     }, 1000)
 };
 
@@ -43,4 +52,21 @@ window.stateChange = function(event) {
 function updateProgressBar(){
     // Update the value of our progress bar accordingly.
     $('#progress-bar').val((player.getCurrentTime() / player.getDuration()) * 100);
+}
+
+function updateTimerDisplay(){
+    // Update current time text display.
+    $('#current-time').text(formatTime( player.getCurrentTime() ));
+    $('#duration').text(formatTime( player.getDuration() ));
+}
+
+function formatTime(time){
+    time = Math.round(time);
+
+    var minutes = Math.floor(time / 60),
+    seconds = time - minutes * 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    return minutes + ":" + seconds;
 }
